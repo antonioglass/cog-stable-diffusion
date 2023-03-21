ad69cfb9c9d25d2eea09d5a726e495a5842e3eff
antonioglass <71437038+antonioglass@users.noreply.github.com>
2023-03-21 16:14:13 +0200

    xformers, prompt weightning (compel)

--- txt2img.py
+++ txt2img.py
@@ -1,29 +1,34 @@
 # Script to run txt2img without Cog
-
 from diffusers import StableDiffusionPipeline
 from diffusers import EulerAncestralDiscreteScheduler
 from diffusers import DPMSolverMultistepScheduler
 import torch
+from compel import Compel
 
-model_id = "./deliberate-v2"
+model_id = "antonioglass/dlbrt"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id)
+compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder)
 
 # change scheduler
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+# enable xformers
 pipe.enable_xformers_memory_efficient_attention()
 
-prompt = "a closeup portrait of a playful maid, undercut hair, apron, amazing body, pronounced feminine feature, busty, kitchen, [ash blonde | ginger | pink hair], freckles, flirting with camera"
-negative_prompt = "(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation"
-height = 1024
-width = 768
-num_inference_steps = 30
+prompt = "a red cat playing with a ball--"
+negative_prompt = "deformed, distorted, disfigured, poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, mutated hands and fingers, disconnected limbs, mutation"
+height = 512
+width = 512
+num_inference_steps = 20
 guidance_scale = 6.5
-seed = 1804518985
+seed = 55
+
+conditioning = compel.build_conditioning_tensor(prompt)
+negative_conditioning = compel.build_conditioning_tensor(negative_prompt)
 
 pipe = pipe.to("cuda")
 generator = torch.Generator("cuda").manual_seed(seed)
 
-# image = pipe(prompt=prompt, negative_prompt=negative_prompt, height=height, width=width, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale,  generator=generator).images[0]
-image = pipe(prompt=prompt, negative_prompt=negative_prompt, height=height, width=width, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
+image = pipe(prompt_embeds=conditioning, negative_prompt_embeds=negative_conditioning, height=height, width=width, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale,  generator=gener>
 image.save("output.png")
